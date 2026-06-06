4877cdf52ce387e0c3ab3a83d489e2e2ee60c588
zkhribach <ziyad.khribach@esi.ac.ma>
2026-06-06 18:29:44 +0100

    feat: initialize Angular backoffice with Material and PWA

---
+++ backoffice/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8080/api',
+  firebase: {
+    apiKey: 'AIzaSyDDNPBl4_kyGJuN9pnSu9Chz1uwn9Mtxc4',
+    authDomain: 'artisan-ecommerce-6a2ee.firebaseapp.com',
+    projectId: 'artisan-ecommerce-6a2ee',
+    storageBucket: 'artisan-ecommerce-6a2ee.firebasestorage.app',
+    messagingSenderId: '638156160483',
+    appId: '1:638156160483:web:ebabc09284f670a682f4d6'
+  }
+};
